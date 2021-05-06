c684913d67a97b8ab34cce435fc254dd86cf9861
Alexander Lee <leeal34uw@gmail.com>
2021-05-06 03:51:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,6 +30,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player(board))
+def move(board, index, first_player(X))
   board[index] = first_player
 end
